db8fa49fe0a7b12198b0b53260804e277960f24f
VVS Phani <seshaphani.v@reportgarden.com>
2019-04-14 01:21:43 +0530

    Removed loggers

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,6 @@
 (function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-  console.log('sayBye', sayBye);
-  console.log('sayHello', sayHello);
-
+  
   for (var i = 0; i < names.length; i ++) {
     var string = names[i].toLowerCase();
     // console.log('string', string);
